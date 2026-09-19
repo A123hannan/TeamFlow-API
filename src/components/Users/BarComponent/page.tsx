03b3a49839a473f85f4b1e655409339fd47e93ff
A123hannan <hannanibrahim609@gmail.com>
2026-09-19 21:36:51 +0500

    Pushing the code of Dashboard and front end page

--- src/components/Users/BarComponent/page.tsx
+++ src/components/Users/BarComponent/page.tsx
@@ -37,7 +37,11 @@ function page() {
             </option>
           ))}
         </select>
-        <select value={city} onChange={(e) => setCity(e.target.value)}>
+        <select
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          className="px-3 py-2 text-sm bg-slate-50 border border-slate-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 text-slate-700"
+        >
           {distinctCities.map((city) => (
             <option value={city} key={city}>
               {city}
